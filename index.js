516c331e37e36f4b27fc86b287bdb2819a82d5af
vaibhavsinghin7-bit <vaibhavsingh.in7@gmail.com>
2026-03-09 12:50:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ client.on("messageCreate", message => {
 });
 
 client.login("MTQ4MDQxMDMxNzA5OTExMDQ5Mg.GZwxLZ.F4br8hy2Sm4JvC8Qmcgb4PEc_kt7ShhdTMl6RE");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+  console.log("Bot is online!");
+});
+
+// Existing code / commands
+client.on("messageCreate", message => {
+  // Your previous code commands
+});
+
+// Add new commands if needed
+client.on("messageCreate", message => {
+  if (message.content === "!ping") message.reply("Pong!");
+});
+
+// Login using the environment variable
+client.login(process.env.DISCORD_BOT_TOKEN);
